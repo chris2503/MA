b318159615d0c0aef4c745f233adf416f3365ce5
Christian Herrmann <christian2.herrmann@tu-dortmund.de>
2018-07-31 04:04:53 +0200

    Trying background estimations

--- Calculations/mymain.py
+++ Calculations/mymain.py
@@ -213,14 +213,14 @@ background = ['coating', 'czt']
 
 
 all_contrib_at116Cd_coating, all_contrib_at130Te_coating = my_main(eventfile_coating, scale_coating, x_range_coating, background[0], Q_val_ret=True)
-my_main(eventfile_czt, scale_czt, x_range_czt, background[1])
+#my_main(eventfile_czt, scale_czt, x_range_czt, background[1])
 sum_glyp_at116Cd = all_contrib_at116Cd_coating[0] + all_contrib_at116Cd_coating[2] + all_contrib_at116Cd_coating[4]
 sum_epox_at116Cd = all_contrib_at116Cd_coating[1] + all_contrib_at116Cd_coating[3] + all_contrib_at116Cd_coating[5]
 sum_glyp_at130Te = all_contrib_at130Te_coating[0] + all_contrib_at130Te_coating[2] + all_contrib_at130Te_coating[4]
 sum_epox_at130Te = all_contrib_at130Te_coating[1] + all_contrib_at130Te_coating[3] + all_contrib_at130Te_coating[5]
 sum_coating_at_116Cd = sum_glyp_at116Cd + sum_epox_at116Cd
 sum_coating_at_130Te= sum_glyp_at130Te + sum_epox_at130Te
-new_data = np.array([[glyptal, epoxy, sum], [sum_epox_at116Cd, sum_glyp_at116Cd, sum_coating_at_116Cd], [sum_epox_at130Te, sum_glyp_at130Te, sum_coating_at_130Te]])
+new_data = np.array([['glyptal', 'epoxy', 'sum'], [sum_epox_at116Cd, sum_glyp_at116Cd, sum_coating_at_116Cd], [sum_epox_at130Te, sum_glyp_at130Te, sum_coating_at_130Te]])
 descriptions = ['Contributions at Qvalues', 'N in 1/kg/keV/yr ']
 var_names = ['Isotope', 'N_at116Cd', 'N_at130Te']
 write_detailed_txtfile(np.transpose(new_data), var_names, descriptions, './calc_solutions/', 'events_at_Qvalues_allcoating.txt' )
